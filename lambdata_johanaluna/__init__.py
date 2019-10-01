de9e7626cb62df388888c5ff13489197473b6e0a
johanaluna <johanaluna@users.noreply.github.com>
2019-09-30 20:37:46 -0400

    add utils

---
+++ lambdata_johanaluna/__init__.py
@@ -0,0 +1,9 @@
+"""
+lambdata - a collection of data science helper functions for lambda school
+"""
+import pandas as pd
+import numpy as np
+
+#sample code
+ONES= pd.DataFrame(np.ones(10))
+ZEROS= pd.DataFrame(np.zeros(50))
