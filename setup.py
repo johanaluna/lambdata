fe63a7ce9b6c1aa62939639c138a033c4f9d28c6
johanaluna <johanaluna@users.noreply.github.com>
2019-10-01 14:11:59 -0400

    final code2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="lambdata-johanaluna",
-    version = "0.1.5",
+    version = "0.1.6",
     author = "johanaluna",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
